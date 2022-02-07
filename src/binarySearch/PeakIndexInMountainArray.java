78e5647dff0eea5195ca3d96086bb874fc0e2369
amrit <amritkumar.b@perfios.com>
2022-02-07 18:16:42 +0530

    some trees and array questions

--- src/binarySearch/PeakIndexInMountainArray.java
+++ src/binarySearch/PeakIndexInMountainArray.java
@@ -2,37 +2,31 @@ package binarySearch;
 class PeakIndexInMountainArray {
 
 	public static void main(String[] args) {
-		int[] arr = {0,1,2,10,5,2};
+		int[] arr = {0,1,2,3,10,6,5,2};
 		PeakIndexInMountainArray mountainArray = new PeakIndexInMountainArray();
 		int index = mountainArray.peakIndexInMountainArray(arr);
 		System.out.println(arr[index]);
 	}
 
-	public int peakIndexInMountainArray(int[] arr) {
+	private int peakIndexInMountainArray(int[] arr) {
 
 		int start = 0;
 		int end = arr.length - 1;
-		return search(arr, start, end);
-	}
-
-	private int search(int[] arr, int start, int end) {
-
+		
 		while (start < end) {
-
 			int mid = start + (end - start)/2;
 
-			System.out.println("mid: " + mid + ", start: " + start + ", end: " + end);
-			if (arr[mid] > arr [mid-1] && (mid -1) >= 0  && arr[mid] > arr [mid+1] && (mid + 1) <= end) {
-				return mid;
-			} else if (arr[mid] > arr [mid-1]){
-				start = mid;
-			} else {
+			if (arr[mid] > arr [mid+1]){
+				// in descending part of the array. This may be the ans, but look at the left too
+				// this is why end is not equal to mid-1
 				end = mid;
+
+			} else {
+				start = mid + 1;
 			}
 		}
-
-		return -1;
-
+		// start equals end
+		return start;
 	}
 }
 
