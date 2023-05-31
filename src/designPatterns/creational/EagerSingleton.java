2b685eed0d27d2973cd0115ddc38cd4e311103ac
amrit.bhuwania <amrit.bhuwania@flexcar.com>
2023-05-31 23:20:03 +0530

    changes

---
+++ src/designPatterns/creational/EagerSingleton.java
@@ -0,0 +1,12 @@
+package designPatterns.creational;
+
+public class EagerSingleton {
+
+    private static EagerSingleton instance = new EagerSingleton();
+
+    private EagerSingleton() {}
+
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
